4e64c734f20e6e5bb990dc9bd7fdfddf9d0a5d79
CristhianCezar <cristhiancezardev@gmail.com>
2023-02-28 05:12:40 -0300

    Home Concluded

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,12 +1,45 @@
 import React from "react";
 import './Home.css';
+import { SlSocialInstagram, SlSocialLinkedin, SlSocialGoogle, SlSocialGithub } from 'react-icons/sl';
+import { BsWhatsapp } from 'react-icons/bs';
 
-export default function Main(){
+export default function Main() {
     return (
         <div className="main">
             <div className="imgMain">
-
-            </div>            
+                <div className="socialMedia">
+                    <ul>
+                        <li>
+                            <a href="#instagram">
+                                <SlSocialInstagram />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#linkedin">
+                                <SlSocialLinkedin />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#gmail">
+                                <SlSocialGoogle />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#github">
+                                <SlSocialGithub />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#whatsapp">
+                                <BsWhatsapp />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="myName">
+                    <h1 className="" id="nameAnimation">Olá, Meu nome é Cristhian Cezar!</h1>
+                </div>
+            </div>
         </div>
     )
 }
